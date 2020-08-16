c682b6783b2ca5dfdcc258c26fa436287154b579
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-16 00:51:04 -0400

    more form submission research

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,41 @@
 //       });
 //     }, false);
 //   })();
+
+
+
+// Cited:  https://www.sitepoint.com/basic-jquery-form-validation-tutorial/
+
+
+// Wait for the DOM to be ready
+// $(function() {
+//     // Initialize form validation on the registration form.
+//     // It has the name attribute "registration"
+//     $("form[name='registration']").validate({
+//       // Specify validation rules
+//       rules: {
+//         // The key name on the left side is the name attribute
+//         // of an input field. Validation rules are defined
+//         // on the right side
+//         name: "required",
+//         phone: "required",
+        
+//           // Specify that email should be validated
+//           // by the built-in "email" rule
+         
+//         message: "required",
+//       },
+//       // Specify validation error messages
+//       messages: {
+//         name: "Please enter your name",
+//         phone: "Please enter your phone",
+//         email: "Please enter a valid email address",
+//         message: "Please enter your message",
+//       },
+//       // Make sure the form is submitted to the destination defined
+//       // in the "action" attribute of the form when valid
+//       submitHandler: function(form) {
+//         form.submit();
+//       }
+//     });
+//   });
