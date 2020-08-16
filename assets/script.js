3cf2f14fc34c5d4a4f89511e71cbb01a77e3f8fa
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-16 19:30:09 -0400

    trying modal displays

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,33 @@
-// // Example starter JavaScript for disabling form submissions if there are invalid fields
-// (function() {
-//     'use strict';
-//     window.addEventListener('load', function() {
-//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//       var forms = document.getElementsByClassName('needs-validation');
-//       // Loop over them and prevent submission
-//       var validation = Array.prototype.filter.call(forms, function(form) {
-//         form.addEventListener('submit', function(event) {
-//           if (form.checkValidity() === false) {
-//             event.preventDefault();
-//             event.stopPropagation();
-//           }
-//           form.classList.add('was-validated');
-//         }, false);
-//       });
-//     }, false);
-//   })();
+$(document).ready(function () {
+
+    // Click listener on submit button to call function
+    $("#submitButton").on("click", function () {
+        event.preventDefault();
+        let name = $(".name").val();
+        let phone = $(".phone").val();
+        let email = $(".email").val();
+        let message = $(".message").val();
+        if (name.length < 1 || phone.length < 12 || email.length < 1 || message.length < 1) {
+            $("alertModal").show();
+        } else $("#successModal").show();
 
 
+        // if (name.length < 1) {
+        //     $("#nameModal").show();
+        // } else if (phone.length < 12) {
+        //     $("nameModal").hide();
+        //     $("#phoneModal").show();
+        // } else if (email.length < 1) {
+        //     $("#emailModal").show();
+        // } else if (message.length < 1) {
+        //     $("#messageModal").show();
+        // } else $("#successModal").show();
 
-// Cited:  https://www.sitepoint.com/basic-jquery-form-validation-tutorial/
+        console.log(name.length);
+        console.log(phone.length);
+        console.log(email.length);
+        console.log(message.length);
+    
+    });
 
-
-// Wait for the DOM to be ready
-// $(function() {
-//     // Initialize form validation on the registration form.
-//     // It has the name attribute "registration"
-//     $("form[name='registration']").validate({
-//       // Specify validation rules
-//       rules: {
-//         // The key name on the left side is the name attribute
-//         // of an input field. Validation rules are defined
-//         // on the right side
-//         name: "required",
-//         phone: "required",
-        
-//           // Specify that email should be validated
-//           // by the built-in "email" rule
-         
-//         message: "required",
-//       },
-//       // Specify validation error messages
-//       messages: {
-//         name: "Please enter your name",
-//         phone: "Please enter your phone",
-//         email: "Please enter a valid email address",
-//         message: "Please enter your message",
-//       },
-//       // Make sure the form is submitted to the destination defined
-//       // in the "action" attribute of the form when valid
-//       submitHandler: function(form) {
-//         form.submit();
-//       }
-//     });
-//   });
+});
