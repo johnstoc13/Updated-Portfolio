17e7e062537435bceabaae57d429ea1c41a1b2ee
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-16 20:42:02 -0400

    final js complete

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,26 @@
 $(document).ready(function () {
 
-    // Click listener on submit button to call function
+    // Click listener to validate field entries
     $("#submitButton").on("click", function () {
         event.preventDefault();
         let name = $(".name").val();
         let phone = $(".phone").val();
         let email = $(".email").val();
         let message = $(".message").val();
-        if (name.length < 1 || phone.length < 12 || email.length < 1 || message.length < 1) {
-            $("alertModal").show();
-        } else $("#successModal").show();
+        let alert = $("#alertModal");
+        let phoneModal = $("#phoneModal");
 
-
-        // if (name.length < 1) {
-        //     $("#nameModal").show();
-        // } else if (phone.length < 12) {
-        //     $("nameModal").hide();
-        //     $("#phoneModal").show();
-        // } else if (email.length < 1) {
-        //     $("#emailModal").show();
-        // } else if (message.length < 1) {
-        //     $("#messageModal").show();
-        // } else $("#successModal").show();
-
-        console.log(name.length);
-        console.log(phone.length);
-        console.log(email.length);
-        console.log(message.length);
-    
+        if (name.length < 1 || email.length < 1 || message.length < 1) {
+            alert.show();
+            phoneModal.hide();
+        } else if (phone.length !== 12) {
+            phoneModal.show();
+            alert.hide();
+        } else {
+            $("#successModal").show();
+            phoneModal.hide();
+            alert.hide();
+        }
     });
-
+    
 });
